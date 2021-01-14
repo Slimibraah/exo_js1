3e0079b0697e90534c66e8c4e95a8e044a28891b
Slimibraah <duclos.salim@gmail.com>
2021-01-14 12:13:49 +0100

    2partie

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
-let prenom = prompt("Quels est votre prenom ?")
+/* let prenom = prompt("Quels est votre prenom ?")
 
-alert("Bonjour " + prenom)
+alert("Bonjour " + prenom) */
+
+let age = prompt("quels âge as tu")
+
+alert("J'ai 20 ans mec")
